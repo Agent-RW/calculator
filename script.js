f5af2540fafdf3f49a19a25e7094ac682cf1108d
Ronald W <RonWingate11@gmail.com>
2024-07-29 23:31:32 -0400

    feat: Add functionality to calculator buttons and display
    Enable users to interact with the calculator buttons to ensure
    functionality before designing function(s) to parse expressions
    in the display

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const DIGIT_BTNS = document.querySelectorAll(".digit");
+const OPERATOR_BTNS = document.querySelectorAll(".operator");
+const EVAL_BTN = document.getElementById("eval-btn");
+const DISPLAY = document.getElementById("display");
 let numA;
 let numB;
 let operator;
@@ -36,3 +40,25 @@ function operate(operator, numA, numB) {
       return "error: invalid operator entered";
   }
 }
+
+function setupCalculator() {
+  DIGIT_BTNS.forEach((digitBtn) => {
+    digitBtn.addEventListener("click", (event) => {
+      console.log(digitBtn.textContent);
+      DISPLAY.textContent += digitBtn.textContent;
+    });
+  });
+  
+  OPERATOR_BTNS.forEach((operatorBtn) => {
+    operatorBtn.addEventListener("click", event => {
+      console.log(operatorBtn.textContent);
+      DISPLAY.textContent += operatorBtn.textContent;
+    });
+  });
+
+  EVAL_BTN.addEventListener("click", event => {
+    console.log(DISPLAY.textContent);
+  })
+}
+
+setupCalculator();
